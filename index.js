668f8a36cfa9fddd18691969a05de4a61c64616b
ErnestMaina <you@example.com>
2023-09-19 06:35:54 +0300

    updated index.js

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 function scuberGreetingForFeet(){
   // Write your code here!
+  if(scuberGreetingForFeet(199) <= 400){ 
+  result='This one is on me!'
+  }
+
+  else if (scuberGreetingForFeet(2001) > 2000){
+  result='I will gladly take your thirty bucks.'
+  }
+
+  else if(scuberGreetingForFeet(2501) > 2500){
+  result='No can do'
+  }
+
+  return result;
 }
 
 function ternaryCheckCity(){
   // Write your code here!
+   city = NYC ? "Ok, sounds good" :  "No go";
+
+ 
 }
 
 function switchOnCharmFromTip(){
   // Write your code here!
+  switch(tip) {
+    case 'generous':
+      return 'Thank you so much.';
+
+     case 'not as generous':
+      return 'Thank you.'
+
+     case  'if anything else':
+      return  'Bye'
+
+  }
+
 }
